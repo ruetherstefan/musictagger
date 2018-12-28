74bf96cee386064e30544b624fec4f8d7e448063
Jan Rüther <jan.ruether@adesso.de>
2018-12-28 19:16:59 +0100

    spulen im song

--- main/pythontagger.py
+++ main/pythontagger.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.togglebutton import ToggleButton
+from kivy.uix.slider import Slider
 from kivy.uix.boxlayout import BoxLayout
 
 import pygame
@@ -21,6 +22,11 @@ def pause_song(instance):
     pygame.mixer.music.pause()
 
 
+def forward_song(instance, motionevent):
+    pygame.mixer.music.rewind()
+    pygame.mixer.music.set_pos(instance.value)
+
+
 class EditScreen(BoxLayout):
 
     def __init__(self, **kwargs):
@@ -30,6 +36,12 @@ class EditScreen(BoxLayout):
 
         self.add_widget(Label(text='Name Lied'))
         self.add_widget(self.erstelle_lied_leiste())
+
+        song_position = Slider(min=0, max=100)
+        song_position.step = 1
+        song_position.bind(on_touch_up=forward_song)
+        self.add_widget(song_position)
+
         self.add_widget(self.erstelle_tag_leiste())
 
     def erstelle_tag_leiste(self):
@@ -62,12 +74,6 @@ class EditScreen(BoxLayout):
         btn3 = Button(text='Pause')
         liedleiste.add_widget(btn3)
         btn3.bind(on_press=pause_song)
-
-        btnminus = Button(text='Minus 10')
-        liedleiste.add_widget(btnminus)
-
-        btnplus = Button(text='Plus 10')
-        liedleiste.add_widget(btnplus)
 
         btn4 = Button(text='Next')
         liedleiste.add_widget(btn4)
