07058ae0150b4b2eefc75268098e252f2f54f5b5
Jan Rüther <jan.ruether@adesso.de>
2019-01-01 13:48:56 +0100

    tags werden gespeichert

--- main/pythontagger.py
+++ main/pythontagger.py
@@ -2,9 +2,7 @@ from kivy.app import App
 from kivy.properties import StringProperty, NumericProperty
 
 from kivy.uix.togglebutton import ToggleButton
-from kivy.uix.slider import Slider
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.widget import Widget
 
 import pygame
 
@@ -14,9 +12,11 @@ from main.songdirectory import SongDirectory
 songdirectory = SongDirectory()
 
 
-class EditScreen(Widget):
+class EditScreen(BoxLayout):
     song_titel = StringProperty()
     song_length = NumericProperty()
+
+    tags = []
 
     def __init__(self, **kwargs):
         super(EditScreen, self).__init__(**kwargs)
@@ -24,23 +24,31 @@ class EditScreen(Widget):
         self.song_titel = songdirectory.get_current_song_name()
         self.song_length = songdirectory.get_currend_song_length()
 
-        #self.add_widget(self.erstelle_tag_leiste())
-        #self.add_widget(self.erstelle_tag_leiste2())
-        #self.add_widget(self.erstelle_tag_leiste3())
+        self.add_widget(self.erstelle_tag_leiste())
+        self.add_widget(self.erstelle_tag_leiste2())
+        self.add_widget(self.erstelle_tag_leiste3())
 
     def prev_song(self):
         pygame.mixer.music.stop()
 
-        songdirectory.save_id3()
+        self.save_tags()
         songdirectory.dec_watched_song_index()
         self.lade_lied()
 
     def next_song(self):
         pygame.mixer.music.stop()
 
-        songdirectory.save_id3()
+        self.save_tags()
         songdirectory.inc_watched_song_index()
         self.lade_lied()
+
+    def save_tags(self):
+        tagstring = ""
+        for button in self.tags:
+            if button.state == "down":
+                tagstring += button.text
+        songdirectory.save_comment(tagstring)
+
 
     def lade_lied(self):
         songdirectory.lade_lied()
@@ -57,7 +65,6 @@ class EditScreen(Widget):
         if widget.collide_point(*touch.pos):
             pygame.mixer.music.rewind()
             pygame.mixer.music.set_pos(widget.value)
-
 
     def erstelle_tag_leiste(self):
         tag_leiste = BoxLayout(spacing=5)
@@ -95,9 +102,9 @@ class EditScreen(Widget):
         return tag_leiste
 
     def erstelle_tag_button(self, name):
-        btn1 = ToggleButton(text=name)
-        btn1.bind(state=lambda o, val: songdirectory.tags.set(name, val == "down"))
-        return btn1
+        btn = ToggleButton(text=name)
+        self.tags.append(btn)
+        return btn
 
 
 class PythontaggerApp(App):
--- main/songdirectory.py
+++ main/songdirectory.py
@@ -1,12 +1,9 @@
 from os import listdir, remove
-import math
 from shutil import copyfile
 
 from pydub import AudioSegment
 import pygame
 import taglib
-
-from main.tags import Tags
 
 
 PLAYCOPY = 'playcopy'
@@ -19,19 +16,15 @@ class SongDirectory():
 
     directory_files = listdir(song_ordner)
 
-    tags = Tags()
-
     def inc_watched_song_index(self):
         self.watched_song_index += 1
         if self.watched_song_index == len(self.directory_files):
             self.watched_song_index = 0
-        self.tags = Tags()
 
     def dec_watched_song_index(self):
         self.watched_song_index -= 1
         if self.watched_song_index == -1:
             self.watched_song_index = len(self.directory_files) - 1
-        self.tags = Tags()
 
     def get_currend_song_path(self):
         return self.song_ordner + PLAYCOPY + str(self.watched_song_index) + '.mp3'
@@ -42,17 +35,6 @@ class SongDirectory():
     def get_current_song_name(self):
         return str(self.directory_files[self.watched_song_index])
 
-    def get_song_length(self):
-        #audiofile = eyed3.load(self.get_currend_song_path())
-        #return int(math.floor(audiofile.info.time_secs))
-        return 42
-
-    def save_id3(self):
-        pass
-#audiofile = eyed3.load(self.get_currend_song_path())
-        #audiofile.tag.publisher = self.tags.create_id3()
-        #audiofile.tag.save()
-
     def lade_lied(self):
         if self.get_current_song_real_path().endswith(".mp3"):
             copyfile(self.get_current_song_real_path(), self.get_currend_song_path())
@@ -61,9 +43,6 @@ class SongDirectory():
 
         pygame.mixer.music.load(self.get_currend_song_path())
         pygame.mixer.music.play()
-
-        #self.song_titel = str(self.directory_files[self.watched_song_index])
-        #App.get_running_app().root.song_titel = str(self.directory_files[self.watched_song_index])
 
     def delete_all_playcopys(self):
         directory_files_with_copies = listdir(self.song_ordner)
@@ -76,4 +55,8 @@ class SongDirectory():
         song = taglib.File(self.get_current_song_real_path())
         return song.length
 
+    def save_comment(self, tagstring):
+        song = taglib.File(self.get_current_song_real_path())
+        song.tags["COMMENT"] = [tagstring]
+        song.save()
 
