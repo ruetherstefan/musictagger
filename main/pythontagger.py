1581edabf3547a605cbcb69846482bdf6f70c89d
Jan Rüther <jan.ruether@adesso.de>
2020-07-26 12:58:02 +0200

    WeddingsTagging

--- main/pythontagger.py
+++ main/pythontagger.py
@@ -24,11 +24,22 @@ class EditScreen(BoxLayout):
         self.song_titel = songdirectory.get_current_song_name()
         self.song_length = songdirectory.get_currend_song_length()
 
+        #self.generate_hardstyle_tagging_buttons()
+        self.generate_wedding_tagging_buttons()
+
+        self.lade_tags()
+
+    def generate_wedding_tagging_buttons(self):
+        self.add_widget(self.erstelle_energy_leiste())
+        self.add_widget(self.erstelle_tag_leiste_genre2())
+        self.add_widget(self.erstelle_tag_leiste_genre3())
+        self.add_widget(self.erstelle_tag_leiste_genre4())
+
+    def generate_hardstyle_tagging_buttons(self):
         self.add_widget(self.erstelle_tag_leiste())
         self.add_widget(self.erstelle_tag_leiste2())
         self.add_widget(self.erstelle_tag_leiste_quellmusik())
         self.add_widget(self.erstelle_tag_leiste_genre())
-        self.lade_tags()
 
     def prev_song(self):
         pygame.mixer.music.stop()
@@ -77,16 +88,13 @@ class EditScreen(BoxLayout):
             pygame.mixer.music.rewind()
             pygame.mixer.music.set_pos(widget.value)
 
+# Hardstyle Tags
+
     def erstelle_tag_leiste(self):
         tag_leiste = BoxLayout(spacing=5)
 
-        rawstyle = self.erstelle_tag_button('rawstyle')
-        rawstyle.group = 'genre'
-        tag_leiste.add_widget(rawstyle)
-        euphoric = self.erstelle_tag_button('euphoric')
-        euphoric.group = 'genre'
-        tag_leiste.add_widget(euphoric)
-
+        tag_leiste.add_widget(self.erstelleGroupButton('rawstyle', 'genre'))
+        tag_leiste.add_widget(self.erstelleGroupButton('euphoric', 'genre'))
         tag_leiste.add_widget(self.erstelle_tag_button('fairy'))
         tag_leiste.add_widget(self.erstelle_tag_button('dunkel'))
         tag_leiste.add_widget(self.erstelle_tag_button('happy'))
@@ -130,6 +138,59 @@ class EditScreen(BoxLayout):
         btn = ToggleButton(text=name)
         self.tags.append(btn)
         return btn
+
+# Weddings Tags
+
+    def erstelle_energy_leiste(self):
+        tag_leiste = BoxLayout(spacing=5)
+
+        tag_leiste.add_widget(self.erstelleGroupButton('tanzen', 'zweck'))
+        tag_leiste.add_widget(self.erstelleGroupButton('entspannen', 'zweck'))
+
+        tag_leiste.add_widget(self.erstelleGroupButton('eGering', 'enegie'))
+        tag_leiste.add_widget(self.erstelleGroupButton('eMittel', 'enegie'))
+        tag_leiste.add_widget(self.erstelleGroupButton('eHoch', 'enegie'))
+
+        return tag_leiste
+
+    def erstelle_tag_leiste_genre2(self):
+        tag_leiste = BoxLayout(spacing=6)
+
+        tag_leiste.add_widget(self.erstelle_tag_button('oldies'))
+        tag_leiste.add_widget(self.erstelle_tag_button('roknroll'))
+        tag_leiste.add_widget(self.erstelle_tag_button('80s'))
+        tag_leiste.add_widget(self.erstelle_tag_button('90s'))
+        tag_leiste.add_widget(self.erstelle_tag_button('schlager'))
+        tag_leiste.add_widget(self.erstelle_tag_button('ballermann'))
+
+        return tag_leiste
+
+    def erstelle_tag_leiste_genre3(self):
+        tag_leiste = BoxLayout(spacing=5)
+
+        tag_leiste.add_widget(self.erstelle_tag_button('urban'))
+        tag_leiste.add_widget(self.erstelle_tag_button('rap'))
+        tag_leiste.add_widget(self.erstelle_tag_button('pop'))
+        tag_leiste.add_widget(self.erstelle_tag_button('dance'))
+        tag_leiste.add_widget(self.erstelle_tag_button('house'))
+
+        return tag_leiste
+
+    def erstelle_tag_leiste_genre4(self):
+        tag_leiste = BoxLayout(spacing=5)
+
+        tag_leiste.add_widget(self.erstelle_tag_button('alternative'))
+        tag_leiste.add_widget(self.erstelle_tag_button('rock'))
+        tag_leiste.add_widget(self.erstelle_tag_button('metal'))
+        tag_leiste.add_widget(self.erstelle_tag_button('transition'))
+        tag_leiste.add_widget(self.erstelle_tag_button('auftritt'))
+
+        return tag_leiste
+
+    def erstelleGroupButton(self, tag, genre):
+        button = self.erstelle_tag_button(tag)
+        button.group = genre
+        return button
 
 
 class PythontaggerApp(App):
